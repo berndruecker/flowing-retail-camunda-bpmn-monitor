9d0bf7908e08acf1e7ae60dcf7192bdf2abf29c0
Bernd Rücker <bernd.ruecker@camunda.com>
2017-05-09 19:06:37 +0200

    moved bpmn-monitor into own repository to seperate it from flowing-retail core example

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # flowing-retail-camunda-bpmn-monitor
-BPMN Monitor for the flowing-retail examples, requires Camunda Enterprise Edition
+BPMN Monitor for the flowing-retail examples, requires Camunda Enterprise Edition - so you can only build and run it if you have access to https://docs.camunda.org/get-started/apache-maven/#enterprise-edition-1
---
+++ src/main/java/io/flowing/retail/commands/BpmnMonitorApplication.java
@@ -0,0 +1,31 @@
+package io.flowing.retail.commands;
+
+import org.camunda.bpm.BpmPlatform;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.camunda.consulting.util.LicenseHelper;
+import com.camunda.consulting.util.UserGenerator;
+
+import io.flowing.retail.adapter.FlowingStartup;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableProcessApplication
+public class BpmnMonitorApplication  {
+
+  public static void main(String... args) {
+    SpringApplication.run(BpmnMonitorApplication.class, args);
+
+    FlowingStartup.startup("bpmn-monitor", new BpmnMonitorEventConsumer(), args);
+
+    // now install license and add default users to Camunda to be ready-to-go
+    ProcessEngine engine = BpmPlatform.getDefaultProcessEngine();    
+    LicenseHelper.setLicense(engine);
+    UserGenerator.createDefaultUsers(engine);
+  }
+
+}
